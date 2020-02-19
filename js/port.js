1660d215524137f9142d1b7ebbdbc89b421fed9b
Jason Heap <jasonrheap@gmail.com>
2020-02-18 23:49:19 -0700

    updated

--- js/port.js
+++ js/port.js
@@ -3,6 +3,22 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 
+$("a.scroll-down").on('click', function (e) {
+e.preventDefault();
+$("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500)
+console.log('testin')
+    });
+
+    $("a[href^='#aboutPage']").click(function(e) {
+      e.preventDefault();
+      
+      var position = $($(this).attr("href")).offset().top;
+    console.log(position)
+      $("body, html").animate({
+        scrollTop: position
+      } /* speed */ );
+    });
+
 
 
 
@@ -28,9 +44,9 @@ var marginLeft = 0;
 setInterval(function () {
     $("#walking-manOne").css({ 'margin-left': marginLeft + 'px' })
     marginLeft = marginLeft + 1
-    console.log(marginLeft)
+    // console.log(marginLeft)
     if (marginLeft === 1500) {
-        console.log("reached")
+        // console.log("reached")
         marginLeft = 0;
     }
 }, 20)
@@ -38,9 +54,9 @@ setInterval(function () {
 setInterval(function () {
   $("#walking-manTwo").css({ 'margin-left': marginLeft + 'px' })
   marginLeft = marginLeft + 1
-  console.log(marginLeft)
+  // console.log(marginLeft)
   if (marginLeft === 1500) {
-      console.log("reached")
+      // console.log("reached")
       marginLeft = 0;
   }
 }, 20)
@@ -49,18 +65,18 @@ setInterval(function () {
 setInterval(function () {
   $("#walking-manThree").css({ 'margin-left': marginLeft + 'px' })
   marginLeft = marginLeft + 1
-  console.log(marginLeft)
+  // console.log(marginLeft)
   if (marginLeft === 1500) {
-      console.log("reached")
+      // console.log("reached")
       marginLeft = 0;
   }
 }, 20)
 setInterval(function () {
   $("#walking-manFour").css({ 'margin-left': marginLeft + 'px' })
   marginLeft = marginLeft + 1
-  console.log(marginLeft)
+  // console.log(marginLeft)
   if (marginLeft === 1500) {
-      console.log("reached")
+      // console.log("reached")
       marginLeft = 0;
   }
 }, 20)
